9c29a21d08c7220dec0aa2845edccfe7d39b483e
Tejesh Jidugu <tejeshjidugu@gmail.com>
2025-10-18 15:08:37 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,13 @@ import Tooltip from "./Tooltip";
 
 function App() {
   return (
-    <div style={{ textAlign: "center", marginTop: "100px" }}>
-      <Tooltip text="This is a tooltip">
-        <h2>Hover over me</h2>
+    <div>
+      <Tooltip text="Tooltip for H2">
+        <h2>Hover over me (H2)</h2>
       </Tooltip>
 
-      <hr />
-
-      <Tooltip text="Another tooltip text">
-        <p>Hover over me to see another tooltip</p>
+      <Tooltip text="Tooltip for P">
+        <p>Hover over me too (P)</p>
       </Tooltip>
     </div>
   );
