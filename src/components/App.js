a274f33146d4c4b97ac5c4d4321f822e85f948be
Tejesh Jidugu <tejeshjidugu@gmail.com>
2025-10-18 13:11:40 +0530

    Refactor tooltip examples in App component
    Updated tooltip examples in App component to include tooltips for heading and paragraph.

--- src/components/App.js
+++ src/components/App.js
@@ -3,21 +3,14 @@ import Tooltip from "./Tooltip";
 import "../styles/App.css"; 
 
 export default function App() {
-  return (
-    <div className="app">
-      <h2>Tooltip examples</h2>
-
-      <div style={{ margin: "40px 0" }}>
-        <Tooltip text="This is a tooltip">
-          <button className="example-button">Hover Over me</button>
-        </Tooltip>
-      </div>
-
-      <div style={{ margin: "40px 0" }}>
-        <Tooltip text="Another helpful tip">
-          <span className="example-text">Hover over me to see another tooltip</span>
-        </Tooltip>
-      </div>
+ return (
+    <div>
+      <h2>
+        <Tooltip text="Tooltip for heading">Hover on heading</Tooltip>
+      </h2>
+      <p>
+        <Tooltip text="Tooltip for paragraph">Hover on paragraph</Tooltip>
+      </p>
     </div>
   );
 }
