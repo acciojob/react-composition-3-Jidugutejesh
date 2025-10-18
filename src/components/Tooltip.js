1e7e66bc766f6f261ec163054176501a18612679
Tejesh Jidugu <tejeshjidugu@gmail.com>
2025-10-18 13:09:39 +0530

    Refactor Tooltip component for visibility state
    Refactor tooltip visibility handling to use isVisible state instead of visible.

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,27 +1,16 @@
 import React, { useState } from "react";
 
 export default function Tooltip({ text, children }) {
-  const [visible, setVisible] = useState(false);
-
-  const show = () => setVisible(true);
-  const hide = () => setVisible(false);
+  const [isVisible, setIsVisible] = useState(false);
 
   return (
     <div
       className="tooltip"
-      onMouseEnter={show}
-      onMouseLeave={hide}
-      onFocus={show}
-      onBlur={hide}
-      tabIndex={0}
-      aria-describedby={text ? "tooltip" : undefined}
+      onMouseEnter={() => setIsVisible(true)}
+      onMouseLeave={() => setIsVisible(false)}
     >
       {children}
-      {visible && (
-        <span className="tooltiptext" role="tooltip">
-          {text}
-        </span>
-      )}
+      {isVisible && <div>{text}</div>}
     </div>
   );
 }
