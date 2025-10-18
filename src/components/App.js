08eac9f5f20e74844c81a7601f23518cf9ddde18
Tejesh Jidugu <tejeshjidugu@gmail.com>
2025-10-18 13:02:22 +0530

    Refactor App component to use named export

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
-
 import React from "react";
-import './../styles/App.css';
+import Tooltip from "./Tooltip";
+import "../styles/App.css"; 
 
-const App = () => {
+export default function App() {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+    <div className="app">
+      <h2>Tooltip examples</h2>
 
-export default App
+      <div style={{ margin: "40px 0" }}>
+        <Tooltip text="This is a tooltip">
+          <button className="example-button">Hover Over me</button>
+        </Tooltip>
+      </div>
+
+      <div style={{ margin: "40px 0" }}>
+        <Tooltip text="Another helpful tip">
+          <span className="example-text">Hover over me to see another tooltip</span>
+        </Tooltip>
+      </div>
+    </div>
+  );
+}
