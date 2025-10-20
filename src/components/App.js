a0183bc5a0e9c5fa67e3e02085b739a01e031711
Tejesh Jidugu <tejeshjidugu@gmail.com>
2025-10-20 18:40:18 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,20 @@
 import React from "react";
 import Tooltip from "./Tooltip";
-// Assuming you link to a main CSS file for general styles
+// Assuming you link to your CSS files here or in index.js
 
 function App() {
   return (
     <div style={{ margin: "50px" }}>
       <Tooltip text="This is a tooltip">
-        <h2>Hover over me</h2> {/* Tooltip appears over the h2 */}
+        {/* The component will automatically apply the 'tooltip' class to the div */}
+        <h2>Hover over me</h2>
       </Tooltip>
+      
+      {/* Added a break for separation, matching the visual of the original image */}
+      <hr style={{ border: '1px dotted #ccc', margin: '20px 0' }} />
 
-      <Tooltip text="This is another tooltip">
-        <p>Hover over me to see another tooltip</p> {/* Tooltip appears over the p */}
+      <Tooltip text="Hover over me to see another tooltip">
+        <p>Hover over me to see another tooltip</p>
       </Tooltip>
     </div>
   );
