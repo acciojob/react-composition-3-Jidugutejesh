416f0c0ceb6783c3b0e6dd520ba0edbaa0352476
Tejesh Jidugu <tejeshjidugu@gmail.com>
2025-10-20 18:26:26 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import Tooltip from "./Tooltip";
+// Assuming you link to a main CSS file for general styles
 
 function App() {
   return (
     <div style={{ margin: "50px" }}>
       <Tooltip text="This is a tooltip">
-        <h2>Hover over me</h2>
+        <h2>Hover over me</h2> {/* Tooltip appears over the h2 */}
       </Tooltip>
 
       <Tooltip text="This is another tooltip">
-        <p>Hover over me to see another tooltip</p>
+        <p>Hover over me to see another tooltip</p> {/* Tooltip appears over the p */}
       </Tooltip>
     </div>
   );
