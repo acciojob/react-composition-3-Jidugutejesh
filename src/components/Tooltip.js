b4b265367f2def06dbbc14516028bce989df7e55
Tejesh Jidugu <tejeshjidugu@gmail.com>
2025-10-29 16:43:02 +0530

    Update Tooltip.js

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,27 +1,21 @@
-// Tooltip.js - MODIFIED to match Cypress selectors
 import React, { useState } from "react";
 
-const Tooltip = ({ text, children }) => {
+function Tooltip({ text, children }) {
   const [visible, setVisible] = useState(false);
+  const showTooltip = () => setVisible(true);
+  const hideTooltip = () => setVisible(false);
 
-  // We are NOT rendering the outer 'div.tooltip' here anymore.
-  // We are just cloning the child and adding event handlers.
-  return React.cloneElement(children, {
-      // Add event handlers to the child element (h2 or p)
-      onMouseEnter: () => setVisible(true),  
-      onMouseLeave: () => setVisible(false),
-      
-    }, 
-    // The original children content
-    children.props.children,
-    
-    // The tooltip content, now rendered as a <div> to satisfy the selector '... > div'
-    visible && (
-      <div className="tooltiptext"> {/* Changed <span> to <div> */}
-        {text}
-      </div>
-    )
+  return (
+    <div
+      className="tooltip"
+      onMouseEnter={showTooltip}
+      onMouseLeave={hideTooltip}
+    >
+      {children}
+
+      {visible && <div className="tooltiptext">{text}</div>}
+    </div>
   );
-};
+}
 
 export default Tooltip;
