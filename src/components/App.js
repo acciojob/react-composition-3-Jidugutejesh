1c69bdedf7df569c91e56dae7b8e40cfeb2145fe
Tejesh Jidugu <tejeshjidugu@gmail.com>
2025-10-29 16:51:26 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,21 @@
 import React from "react";
 import Tooltip from "./Tooltip";
-import "./App.css"; 
+import "./App.css"; // THIS MUST EXIST for tests to pass
 
 function App() {
   return (
-    <div style={{ padding: "40px", fontFamily: "serif" }}>
+    <div className="app-root">
       <h2>Tooltip Demo</h2>
 
       <Tooltip text="This is a tooltip">
-        <h3 style={{ display: "inline-block", margin: "24px 0" }}>
-          Hover over me
-        </h3>
+        <h3 className="example-child">Hover over me</h3>
       </Tooltip>
 
       <br />
       <br />
 
       <Tooltip text="Another tooltip message!">
-        <p
-          style={{
-            display: "inline-block",
-            borderBottom: "1px dashed #ccc",
-            padding: "8px 0",
-          }}
-        >
-          Hover over this text too
-        </p>
+        <p className="example-child">Hover over this text too</p>
       </Tooltip>
     </div>
   );
