404dabf31bad40d6cecfa06c01ce6405c729502d
Tejesh Jidugu <tejeshjidugu@gmail.com>
2025-10-29 16:43:40 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,31 @@
-// App.js - MODIFIED to apply the .tooltip class to the children
 import React from "react";
 import Tooltip from "./Tooltip";
-import './Tooltip.css'; // Ensure CSS is imported
+import "./App.css"; 
 
 function App() {
   return (
-    <div style={{ margin: "50px" }}>
-      {/* The Tooltip component will clone this <h2>, 
-        add onMouseEnter/onMouseLeave, and inject the tooltip <div>. 
-        Resulting HTML structure is h2.tooltip > ... + div.tooltiptext. 
-        This satisfies the selectors: h2.tooltip and h2.tooltip > div 
-      */}
-      <Tooltip text="This is a tooltip">
-        <h2 className="tooltip">Hover over me</h2> {/* Added className="tooltip" */}
-      </Tooltip>
-      
-      <hr style={{ border: '1px dotted #ccc', margin: '20px 0' }} />
+    <div style={{ padding: "40px", fontFamily: "serif" }}>
+      <h2>Tooltip Demo</h2>
 
-      {/* Second Tooltip */}
-      <Tooltip text="Hover over me to see another tooltip">
-        <p className="tooltip">Hover over me to see another tooltip</p> {/* Added className="tooltip" */}
+      <Tooltip text="This is a tooltip">
+        <h3 style={{ display: "inline-block", margin: "24px 0" }}>
+          Hover over me
+        </h3>
+      </Tooltip>
+
+      <br />
+      <br />
+
+      <Tooltip text="Another tooltip message!">
+        <p
+          style={{
+            display: "inline-block",
+            borderBottom: "1px dashed #ccc",
+            padding: "8px 0",
+          }}
+        >
+          Hover over this text too
+        </p>
       </Tooltip>
     </div>
   );
